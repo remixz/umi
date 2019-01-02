0d85e726ff8c7d3215d9e7a4b1a3e7724feac880
Julian Bath <julzso23@gmail.com>
2019-01-02 16:08:37 +0000

    Added filter buttons to browse page

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="mt2">
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'popular') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('popular')">Popular</button>
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'alpha') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('alpha')">Alphabetical</button>
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'featured') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('featured')">Featured</button>
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'newest') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('newest')">Newest</button>
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'simulcast') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('simulcast')">Simulcast</button>
+    <button class="f6 mt1 mb4 fw6 ba b--black-20 bg-white bg-animate hover-bg-light-gray black br1 pointer ph3 pv2 tc br2 box-shadow-umi" :class="(filter == 'updated') && 'white bg-dark-blue hover-bg-light-blue'" @click="updateFilter('updated')">Updated</button>
+
     <div v-if="allData.length > 0" class="center container-width">
       <series-item v-for="d in allData" :key="d.series_id" :id="d.series_id" :noMargin="true" />
       <div class="f5 fw6 db ba b--black-20 bg-animate hover-bg-light-gray black br1 pointer pa3 tc more-btn box-shadow-umi" :class="[paginationLoading ? 'bg-light-gray' : 'bg-white']" @click="handlePagination">
@@ -22,13 +29,13 @@
       title: 'Browse'
     },
     data: () => ({
-      data: [],
       paginationLoading: false,
-      offset: 0
+      offset: 0,
+      filter: 'popular'
     }),
     computed: {
       allData () {
-        return this.$store.state.seriesList.concat(this.data)
+        return this.$store.state.seriesList
       }
     },
     components: {SeriesItem, LoadingMediaItem},
@@ -36,9 +43,17 @@
       async handlePagination () {
         this.paginationLoading = true
         this.offset += 50
-        const data = await this.$store.dispatch('getSeriesList', {filter: 'popular', offset: this.offset})
-        this.data = this.data.concat(data)
+        await this.$store.dispatch('getSeriesList', {filter: this.filter, offset: this.offset})
         this.paginationLoading = false
+      },
+
+      async updateFilter(filter) {
+        this.filter = filter
+        this.offset = 0
+
+        this.$store.dispatch('clearSeriesList')
+
+        await this.$store.dispatch('getSeriesList', {filter: this.filter, offset: this.offset})
       }
     },
     created () {
